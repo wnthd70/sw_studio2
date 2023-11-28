e06aeb206d6790ccff4e545b3bfcc2484f1699bc
song <wnthd1591@naver.com>
2023-11-28 11:40:19 +0900

    second commit

---
+++ FinalProject/ViewController_bookInfo.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController_bookInfo.swift
+//  FinalProject
+//
+//  Created by 232-11 on 2023/11/28.
+//
+
+import UIKit
+
+class ViewController_bookInfo: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
