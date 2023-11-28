ce288f2d86d0c7a00b98b9aee40a6992cbc732a7
232-11 <nm232-11@232-11ui-Mac-Studio.local>
2023-11-28 09:01:52 +0900

    first commit

---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by 232-11 on 2023/11/21.
+//
+
+import UIKit
+
+
+
+class ViewController: UIViewController {
+    var startTime = Date()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
